0f630a0e644eb034439d9f21a985e082e7117c66
Erik Alves <dev.eriksantos@gmail.com>
2023-08-22 14:28:11 -0300

    last version

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
-function toggleTheme(){
-    const body = document.body
-    body.classList.toggle('light')
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle("light");
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("light")) {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    }
+});
